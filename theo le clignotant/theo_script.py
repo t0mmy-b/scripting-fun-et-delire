fff87bfbe87b77efab8dc316cd5a50a972e91f4c
t0mmy-b <35820420+t0mmy-b@users.noreply.github.com>
2018-09-13 22:50:12 +0200

    Update theo_script.py
    Bold effect

--- theo le clignotant/theo_script.py
+++ theo le clignotant/theo_script.py
@@ -118,7 +118,7 @@ c = 0
 colors = ['\033[97m','\033[96m','\033[95m','\033[94m','\033[93m','\033[92m','\033[91m']
 
 while 1:
-	print(colors[c]+ascii_art)
+	print("\033[1m"+colors[c]+ascii_art)
 	c += 1
 	if c == len(colors):
 		c = 0
